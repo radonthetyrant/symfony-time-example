a84f91a06a29c3b63531b0b3cd94a53ee9e8131f
radonthetyrant <radon@rn86.de>
2021-09-14 00:52:17 +0200

    update init script

--- bin/init_all.sh
+++ bin/init_all.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 docker-compose up -d
+docker-compose exec php composer install
 docker-compose exec php bin/console doctrine:database:drop -f
 docker-compose exec php bin/console doctrine:database:create --if-not-exists
 docker-compose exec php bin/console doctrine:migration:migrate -n
