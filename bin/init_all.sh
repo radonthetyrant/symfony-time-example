4f0fc85a363353701250508f313f52dd984a4a48
radonthetyrant <radon@rn86.de>
2021-09-13 19:18:37 +0200

    add init helper

---
+++ bin/init_all.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+docker-compose up -d
+docker-compose exec php bin/console doctrine:database:drop -f
+docker-compose exec php bin/console doctrine:database:create --if-not-exists
+docker-compose exec php bin/console doctrine:migration:migrate -n
+docker-compose exec php bin/console doctrine:fixtures:load -n
+docker-compose exec php bin/console cache:clear
+docker-compose exec php bin/console cache:clear --env=prod
