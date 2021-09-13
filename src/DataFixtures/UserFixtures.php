43e13c192d4b745838e8392632e28192d9173b5c
radonthetyrant <radon@rn86.de>
2021-09-13 18:18:22 +0200

    add user fixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(private UserPasswordEncoderInterface $passwordEncoder)
+    {
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $users = [
+            'user' => 'password',
+        ];
+
+        foreach ($users as $username => $password) {
+            $user = (new User())
+                ->setUsername($username)
+                ->setRoles(['ROLE_USER']);
+            $user->setPassword($this->passwordEncoder->encodePassword($user, $password));
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
